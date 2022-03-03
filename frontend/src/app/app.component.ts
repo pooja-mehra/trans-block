8bd15eafb201a20bf1723259485802cc6b6ef056
pooja <p.mehra86@gmail.com>
2022-03-03 10:23:22 -0800

    added default web3 provider for development mode

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,12 +4,15 @@ import {DashboardService} from './services/dashboard.service'
 import { Topics } from './shared/topics'
 declare let window: any;
 const { ethereum } = window;
+import { environment} from '../environments/environment'
+const default_web3_provider = environment.DEFAULT_WEB3_PROVIDER;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit{
   title = 'frontend';
   web3Provider:any;
@@ -17,18 +20,22 @@ export class AppComponent implements OnInit{
   account:any;
   topics:Topics[] =[];
   test:Topics[]=[];
-
+  
   constructor(private dashboardService:DashboardService){
+   
     try {
       window.web3.currentProvider;
       this.web3Provider = window.web3.currentProvider;
       ethereum.web3 = new Web3(this.web3Provider);
       this.web3 = ethereum.web3;
-      this.getTopics();
     } catch(e) {
-      e = 'Install Metamask'
+      e = 'Install Metamask for write access'
       alert(e);
+      this.web3Provider = new Web3.providers.HttpProvider(default_web3_provider);
+      window.web3 = new Web3(this.web3Provider);
+      this.web3 = window.web3;
     }
+    this.getTopics();
   }
 
   ngOnInit(){
@@ -36,13 +43,18 @@ export class AppComponent implements OnInit{
   }
 
   async getAccount(){
-    let accounts = await ethereum.request({ method: 'eth_accounts' });
-    this.account =accounts[0];
+    try{
+      let accounts = await ethereum.request({ method: 'eth_accounts' });
+      this.account = accounts[0];
+    }
+    catch(e){
+      this.account = undefined;
+    }
   }
 
   async getTopics(){
-    await this.getAccount();
     let that = this
+    await this.getAccount();
     this.dashboardService.getTopics(this.web3Provider).then(function (result:any){
       that.topics = result.result;
       result.result.forEach(element => {
